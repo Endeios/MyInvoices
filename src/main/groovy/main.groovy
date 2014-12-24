1e4abd1f97b3ff09a8e4ee3270aa7110a8de798b
endeios@gmail.com <endeios@gmail.com>
2014-12-24 11:31:34 +0100

    reindent

--- src/main/groovy/main.groovy
+++ src/main/groovy/main.groovy
@@ -29,64 +29,64 @@ def tabs = [
 	]
 
 start{
-     stage(title:"Welcome",visible:true,id:"stage"){
-	     scene(fill:BLACK, width:500,height:300,id:"scene"){
-	       borderPane{
-	          top{
-                     menuBar{
-		     	menu("File"){
-			    menuItem("Print Invoices",onAction:{evt->
-				PrinterJob pj = PrinterJob.getPrinterJob();
-				//PrinterJob pj = PrinterJob.createPrinterJob();
-				boolean doPrint = pj.printDialog();
-				//boolean doPrint = pj.showPrintDialog(stage)
-				if(doPrint){
-                  InputStream input = new FileInputStream(new File("/home/bveronesi/reportBusiness.jrxml"));
-                  JasperDesign design = JRXmlLoader.load(input);
-                  JasperReport report = JasperCompileManager.compileReport(design);
-                  def data = new JRBeanCollectionDataSource([test.invoice])
-                  println data
-                  JasperPrint myPrint = JasperFillManager.fillReport(report,[:] ,data)
+    stage(title:"Welcome",visible:true,id:"stage"){
+        scene(fill:BLACK, width:500,height:300,id:"scene"){
+            borderPane{
+                top{
+                    menuBar{
+                        menu("File"){
+                            menuItem("Print Invoices",onAction:{evt->
+                                PrinterJob pj = PrinterJob.getPrinterJob();
+                                //PrinterJob pj = PrinterJob.createPrinterJob();
+                                boolean doPrint = pj.printDialog();
+                                //boolean doPrint = pj.showPrintDialog(stage)
+                                if(doPrint){
+                                    InputStream input = new FileInputStream(new File("/home/bveronesi/reportBusiness.jrxml"));
+                                    JasperDesign design = JRXmlLoader.load(input);
+                                    JasperReport report = JasperCompileManager.compileReport(design);
+                                    def data = new JRBeanCollectionDataSource([test.invoice])
+                                    println data
+                                    JasperPrint myPrint = JasperFillManager.fillReport(report,[:] ,data)
+    
+                                    println pj					
+                                    PDDocument pdf = PDDocument.load("/home/bruno/Scaricati/pragmatic-guide-to-sass_p1_0.pdf")
+                                    pj.setPageable(new PDPageable(pdf, pj))
+                                    pj.setJobName("InvoicesXXXXXXX");
+                                    try {
+                                        pj.print();
+                                    } catch (PrinterException e) {
+                                        System.out.println(e);
+                                    }
+                              }
+                            })
+                        }
+                    }
+                }
+                center{
+                    tabPane(){
+                        tabs.each{aTab ->
+                            tab(aTab.title,closable:false){
+                                label(aTab.label)
+                                tableView(selectionMode: "single", cellSelectionEnabled: true, editable: true, items: aTab.crud_service.findAll()){
+                                    aTab.cols.each{colName->
+                                        tableColumn(editable:true,property:colName,text:colName,
+                                        onEditCommit: { event ->
+                                            def item = event.tableView.items.get(event.tablePosition.row)
+                                            item[colName] = event.newValue;
+                                        aTab.crud_service.save(item)
+                                        //event.tableView.items = aTab.service.findAll()
+                                        }
+                                        )
+                                    }
+                                }
 
-				  println pj					
-				  PDDocument pdf = PDDocument.load("/home/bruno/Scaricati/pragmatic-guide-to-sass_p1_0.pdf")
-				  pj.setPageable(new PDPageable(pdf, pj))
-				  pj.setJobName("InvoicesXXXXXXX");
-    				  try {
-				    pj.print();
-				  } catch (PrinterException e) {
-				    System.out.println(e);
-				  }
-				}
-			    })
-			}
-		     }
-		  }
-		  center{
-		     tabPane(){
-		        tabs.each{aTab ->
-			   tab(aTab.title,closable:false){
-				label(aTab.label)
-				tableView(selectionMode: "single", cellSelectionEnabled: true, editable: true, items: aTab.crud_service.findAll()){
-				  aTab.cols.each{colName->
-				    tableColumn(editable:true,property:colName,text:colName,
-				      onEditCommit: { event ->
-				     	def item = event.tableView.items.get(event.tablePosition.row)
-				    	item[colName] = event.newValue;
-					aTab.crud_service.save(item)
-					//event.tableView.items = aTab.service.findAll()
-				      }
-				    )
-				  }
-				}
-
-			   }
-			}
-		     }
-	          }
-		}
-	     }
-     }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
 }
 
 
